9d50a0a4ad643f6962ae507b21f6b7f235cde488
Tuan Le Thanh <tuanitpro@gmail.com>
2021-08-11 23:32:03 +0700

    Init Project

---
+++ src/ios/Pods/Target Support Files/Pods-hello_reactnative-hello_reactnativeTests/Pods-hello_reactnative-hello_reactnativeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_hello_reactnative_hello_reactnativeTests : NSObject
+@end
+@implementation PodsDummy_Pods_hello_reactnative_hello_reactnativeTests
+@end
